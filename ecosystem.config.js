c7fc6a03bc1e3a5f3e9f5e13bed010ad38fd1492
Allen <1942813644@qq.com>
2022-03-28 09:45:02 +0800

    修改配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: 'root',
+      user: 'ubuntu',
       host: '42.192.91.89',
       ref: 'origin/main',
-      repo: 'git@git.zhlh6.cn:Nick930826/juejue-vite-h5.git',
+      repo: 'https://github.com/Allen-XiaoDeng/wangcai-react.git',
       path: '/workspace/wangcai-react',
       'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
