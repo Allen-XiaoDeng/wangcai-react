cadcb4d9c1bf59dede6b3865310956e200bc0549
Allen <1942813644@qq.com>
2022-03-28 10:56:25 +0800

    modified ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,8 @@ module.exports = {
       user: 'ubuntu',
       host: '42.192.91.89',
       ref: 'origin/main',
-      repo: 'https://github.com/Allen-XiaoDeng/wangcai-react.git',
+      repo: 'git@github.com:Allen-XiaoDeng/wangcai-react.git',
+      fetch: 'all',
       path: '/workspace/wangcai-react',
       'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
